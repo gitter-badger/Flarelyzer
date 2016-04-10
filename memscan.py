b4e5442f8ac3c964b2ac992e82022914e2be7950
Javier Acosta <javieracost2@gmail.com>
2016-04-10 18:16:29 -0430

    Further performance improvements and way less CPU usage.
     Changes:
    
    *  Now only the program's heap is scanned, since it is where such dynamically allocated data is stored.
    *  Moved the memory region updater to a thread separated from the message parsing function, resulting in less CPU usage while keeping a fast response time
    *  Fixed a couple of bugs that could make the program crash under certain circumstances
    *  Various small improvements in the code
    
    Also modified and improved the error reporting to console

--- memscan.py
+++ memscan.py
@@ -1,18 +1,36 @@
-import sys
 import socket
-from subprocess import check_output, CalledProcessError
 import signal
-#import time
-#TODO: stop opening and closing files with every iteration
+from time import sleep
+from thread import start_new_thread
+from traceback import print_exc as stacktrace
+from subprocess import check_output, CalledProcessError
 
 cached_loot_messages = set()
+heap = tuple()
 notifier = None
+
+
+def update_heap(pid):
+	global heap
+	while True:
+		maps_file = open('/proc/%s/maps' % pid, 'r')
+		for line in maps_file.readlines():  # for each mapped region
+			mem_info = line.rsplit()
+			mapping = mem_info[-1]
+			if mapping == '[heap]':  # reading only from the heap, all the messages are allocated there!
+				region = mem_info[0].split('-')
+				start = int(region[0], 16)
+				end = int(region[1], 16)
+				if (start, end) != heap:  # check for heap expansions
+					heap = (start, end)
+		maps_file.close()
+		sleep(1)
 
 
 def is_timestamp(string):
 	return string[2] == ':' and string[:2].isdigit() and string[3:5].isdigit() and string[5] == ' '
 
-#@profile
+
 def messages(chunk):
 	index = 0
 	while index < len(chunk) - 6:  # No point in reading something shorter than '00:00 '
@@ -23,48 +41,45 @@ def messages(chunk):
 		else:
 			index += 1
 
-#@profile
+
 def read_process_memory(pid):
+	global heap
+	mem_file = open('/proc/%s/mem' % pid, 'r')
 	item_drops = []
 	exp = dict()
-	maps_file = open("/proc/%s/maps" % pid, 'r')
-	mem_file = open("/proc/%s/mem" % pid, 'r')
 	try:
-		for line in maps_file.readlines():  # for each mapped region
-			mapping = line.rsplit(' ')[-1]
-			if (line[line.find(' ')+1] == 'r' and               # if this is a readable region
-				mapping not in ['(deleted)\n', '[vdso]\n'] and  # and not a shared object,
-				mapping[0] != '/'):                             # or mapped file
-				region = line.split()[0].split('-')
-				start = int(region[0], 16)
-				end = int(region[1], 16)
-				mem_file.seek(start)  # seek to region start
-				try: chunk = mem_file.read(end - start)  # read region contents
-				except: continue
-				for log_message in messages(chunk):
-					#print log_message
-					if log_message[5:14] == ' Loot of ':
-						global cached_loot_messages
-						if log_message in cached_loot_messages:
-							continue
-						cached_loot_messages.add(log_message)
-						item_drops.append(log_message)
-					elif log_message[5:17] == ' You gained ':
-						t = log_message[0:5]
-						try:
-							e = int(log_message[17:log_message.find(' ')])
-							if t not in exp: exp[t] = e
-							else: exp[t] += e
-						except: pass
+		if not heap:
+			start_new_thread(update_heap, (pid,))
+			while not heap:
+				sleep(0.2)
+		start, end = heap
+		mem_file.seek(start)
+		try:
+			chunk = mem_file.read(end - start)  # read region contents
+		except:
+			chunk = ''
+		if messages(chunk):
+			for log_message in messages(chunk):
+				if log_message[5:14] == ' Loot of ':
+					global cached_loot_messages
+					if log_message in cached_loot_messages:
+						continue
+					cached_loot_messages.add(log_message)
+					item_drops.append(log_message)
+				elif log_message[5:17] == ' You gained ':
+					t = log_message[0:5]
+					try:
+						e = int(log_message[17:log_message.find(' ')])
+						if t not in exp: exp[t] = e
+						else: exp[t] += e
+					except: pass
 	except KeyboardInterrupt:
 		return dict()
-	except Exception, e:
-		exc_type, exc_obj, exc_tb = sys.exc_info()
-		print str(e)
-		print 'while reading memory: ' + str(exc_type) + ' - at line: ' + str(exc_tb.tb_lineno)
+	except:
+		print 'Error while reading memory!'
+		stacktrace()
 		return
 	finally:
-		maps_file.close()
 		mem_file.close()
 
 	return_values = dict()
@@ -78,7 +93,7 @@ def quit():
 		notifier.close()
 	except: pass
 	print '--Memory scanner closed--'
-	sys.exit(0)
+	exit(0)
 
 signal.signal(signal.SIGTERM, lambda x, y: quit)
 
@@ -95,36 +110,32 @@ def main():
 		print 'Client PID: ' + str(tibiaPID)
 	except socket.error:
 		print 'Unable to connect to notification agent!'
-		pass
 	except CalledProcessError:
 		print 'Tibia client not found!'
-		pass
-	except Exception, e:
-		print 'Unexpected error ' + str(e)
+	except:
+		print 'Unexpected error'
+		stacktrace()
 	else:
 		# we're good to go
 		print '===Memory analyzer started==='
 		notifier.sendall('ATTACHED')
 		while True:
 			try:
-				#iniT = time.time()
 				res = read_process_memory(tibiaPID)
-				#spent = time.time() - iniT
 				if not res:
 					quit()
 				for full_msg in res['item_drops']:
 					notifier.sendall(full_msg)
 					notifier.recv(8)
-				#print 'time spent reading memory: ', spent
 				notifier.sendall('NEXT')
 				response = notifier.recv(8)
 				if response == 'QUIT':
 					quit()
-			except Exception, e:
-				exc_type, exc_obj, exc_tb = sys.exc_info()
-				print str(e)
-				print str(exc_type) + ' - at line: ' + str(exc_tb.tb_lineno)
+				sleep(0.1)
+			except:
+				stacktrace()
 				quit()
+			sleep(0.1)
 	print '==Aborting=='
 	exit(1)
 
